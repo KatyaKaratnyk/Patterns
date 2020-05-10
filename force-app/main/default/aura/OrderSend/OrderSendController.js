7fee7bc574cc95e9cd5ade903cc1b384f3889e10
Kateryna Karatnyk <kkara@softserveinc.com>
2020-05-10 18:49:13 +0300

    Add State Pattern

--- force-app/main/default/aura/OrderSend/OrderSendController.js
+++ force-app/main/default/aura/OrderSend/OrderSendController.js
@@ -1,6 +1,6 @@
 ({
     doSend : function(component, event, helper) {
-        helper.doSend(component, event, helper);
+        helper.doSend(component);
     },
     doCancel: function (component, event, helper) {
         helper.doCancel(component);
--- force-app/main/default/aura/OrderSend/OrderSendHelper.js
+++ force-app/main/default/aura/OrderSend/OrderSendHelper.js
@@ -1,9 +1,38 @@
 ({
-    doSend : function() {
+    doSend : function(component) {
+        let recordId = component.get("v.recordId");
+        let action = component.get("c.sendOrder");
+        
+        action.setParams({
+            orderId: recordId
+        });
 
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            let resp = response.getReturnValue();
+            if (state == "SUCCESS") {
+                this.fireToast("success", "Order was sent succesfully");
+            } else {
+                this.fireToast("error", "Order wasn`t sent");
+            }
+            component.set("v.loadingInProgress", false);
+        });
+
+        $A.enqueueAction(action);
     },
 
     doCancel: function (component) {
         $A.get("e.force:closeQuickAction").fire();
+    },
+
+    fireToast: function (type, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            mode: "dismisable",
+            message: message,
+            type: type,
+            duration: 5000
+        });
+        toastEvent.fire();
     },
 })
